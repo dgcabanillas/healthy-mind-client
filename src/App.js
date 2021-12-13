935be2633a586453cafff02cb2d72593b970bcca
dgcabanillas <dg.cabanillas@uni.pe>
2021-12-13 15:35:04 -0500

    feature(mui-theme): Creando un tema para la plataforma

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import AppRouter from "./routers/AppRouter";
+import { ThemeProvider  } from '@mui/material/styles';
+import { theme } from './utils/theme';
 
 const App = () => {
   return (
-    <div>
+    <ThemeProvider theme={theme}>
       <AppRouter />
-    </div>
+    </ThemeProvider >
   );
 }
 
