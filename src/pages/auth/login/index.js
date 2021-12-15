757460a55b5b4f7a2a3b9d1742fc86a50a151dcf
REYNALDO <reynaldo.quispe.27@unsch.edu.pe>
2021-12-14 19:57:12 -0500

    feature:login-part1-rqs

--- src/pages/auth/login/index.js
+++ src/pages/auth/login/index.js
@@ -1,11 +1,55 @@
+import { Avatar, Grid, Paper } from '@mui/material';
 import React from 'react'
-import AuthLayout from '../../../components/layouts/AuthLayout';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import { TextField } from '@mui/material';
+import { FormControlLabel } from '@mui/material';
+import { Checkbox } from '@mui/material';
+import { Button } from '@mui/material';
+import { Typography } from '@mui/material';
+import { Link } from '@mui/material';
+
 
 const Login = () => {
+
+  const paperStyle={padding: 10,height:'70vh', width:280,margin:"90px auto "}
+  const avatarStyle={backgroundColor:'purple'}
+  const btnStyle={backgroundColor:'purple'}
+  //const subPaperStyle={padding: 10,height:'40vh', width:400,margin:"0px auto "}
+
   return (
-    <AuthLayout>
-      login
-    </AuthLayout>
+    <Grid>
+      <Paper elevation={10} style={paperStyle}>
+        <Grid align="center">
+          <Avatar style={avatarStyle}><LockOutlinedIcon/></Avatar>
+          <h1>sign in</h1>
+        </Grid>
+        <TextField label="Username" placeholder='Ingrese el usuario' id="standard-basic"  variant="standard" fullWidth required></TextField>
+        <TextField label="Password" placeholder='Ingrese la contraseña' id="standard-basic"  variant="standard" type='password' fullWidth required></TextField> 
+
+        <FormControlLabel
+
+          control={
+            <Checkbox
+             name="checked8"
+             color="primary"
+            /> 
+          }
+          label="Recordar"
+        />
+        <Button type='submit' variant='contained' style={btnStyle} color='primary' fullWidth>Ingresar</Button>
+        <Typography> 
+          <Link href="#">
+            ¿Te olvidaste tu contraseña?
+          </Link>
+        </Typography>
+        <Typography> ¿Tu tienes una cuenta?
+          <Link href="#">
+              Ingresar
+          </Link>
+        </Typography>
+      </Paper>
+
+    </Grid> 
   )
 }
 
